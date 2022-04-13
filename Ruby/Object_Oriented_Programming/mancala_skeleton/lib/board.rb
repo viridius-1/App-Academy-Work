24f5777eff9b7ed266a3fa88b41af2d0a5665f26
Hornet-Developer <hornet.dev.0802@gmail.com>
2022-04-12 19:28:45 -0700

    Add methods to board.rb
    make_move, distribute_stones, remove_stones, update_points_store_idx, next_turn

--- Ruby/Object_Oriented_Programming/mancala_skeleton/lib/board.rb
+++ Ruby/Object_Oriented_Programming/mancala_skeleton/lib/board.rb
@@ -3,11 +3,13 @@ require 'byebug'
 class Board
 
   attr_reader :name1, :name2
-  attr_accessor :cups
+  attr_accessor :cups, :opponent_points_store_idx, :player_points_store_idx
 
   def initialize(name1, name2)
     @name1 = name1
     @name2 = name2 
+    @player_points_store_idx = nil 
+    @opponent_points_store_idx = nil 
     @cups = Array.new(14) { Array.new([]) }
     place_stones
   end
@@ -25,13 +27,59 @@ class Board
   def valid_move?(start_pos)
       raise 'Invalid starting cup' if invalid_position?(start_pos)
       raise 'Starting cup is empty' if cups[start_pos].empty?  
+      true 
   end
 
+  def update_points_store_idx(player_name)
+    if player_name == name1
+      @player_points_store_idx = 6
+      @opponent_points_store_idx = 13 
+    else 
+      @player_points_store_idx = 13 
+      @opponent_points_store_idx = 6 
+    end 
+  end 
+
+  def remove_stones(position)
+    cups[position] = []
+  end 
+
+  def distribute_stones(stones_to_distribute, start_pos)
+    next_position_hash = { 0 => 1, 1 => 2, 2 => 3, 3 => 4, 4 => 5, 5 => 6, 6 => 7, 7 => 8, 8 => 9, 9 => 10, 10 => 11, 11 => 12, 12 => 13, 13 => 0}
+    starting_idx = next_position_hash[start_pos] 
+
+    cup_idx = starting_idx
+    until stones_to_distribute == 0 
+      if cup_idx != opponent_points_store_idx
+        @cups[cup_idx] << :stone 
+        stones_to_distribute -= 1 
+      end 
+      cup_idx = next_position_hash[cup_idx] if stones_to_distribute > 0 
+    end 
+
+    next_turn(cup_idx)
+  end 
+
   def make_move(start_pos, current_player_name)
+      update_points_store_idx(current_player_name)
+      stones_to_distribute = cups[start_pos].length 
+      remove_stones(start_pos)
+      distribute_stones(stones_to_distribute, start_pos)
   end
 
   def next_turn(ending_cup_idx)
-    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    next_turn_value = nil 
+
+    if ending_cup_idx == player_points_store_idx
+      next_turn_value = :prompt 
+    elsif cups[ending_cup_idx].length == 1 
+      next_turn_value = :switch 
+    elsif cups[ending_cup_idx].length > 1 
+      next_turn_value = ending_cup_idx 
+    end 
+
+    render
+    next_turn_value 
   end
 
   def render

d98235eb6b480e360699d05b692d1b6331fbb0e6
Hornet-Developer <hornet.dev.0802@gmail.com>
2022-04-14 17:28:04 -0700

    Add bonus & all_subordinate_salaries to manager.rb

--- Ruby/Object_Oriented_Programming/class_inheritance/manager.rb
+++ Ruby/Object_Oriented_Programming/class_inheritance/manager.rb
@@ -2,7 +2,7 @@ require_relative 'employee'
 
 class Manager < Employee 
 
-    attr_reader :name 
+    attr_reader :name, :salary 
     attr_accessor :employees
 
     def initialize(name, title, salary, boss)
@@ -12,6 +12,27 @@ class Manager < Employee
 
     def add_employee(employee)
         @employees << employee if employee.boss == name  
+    end 
+
+    def bonus(multiplier)
+        all_subordinate_salaries * multiplier
+    end 
+
+    private 
+
+    def all_subordinate_salaries
+        salary_total = 0 
+
+        workers = [self]
+        until workers.empty?
+            worker = workers.shift 
+            salary_total += worker.salary if worker != self 
+            if worker.class == Manager 
+                worker.employees.each { |worker| workers << worker }
+            end 
+        end 
+
+        salary_total
     end 
 
 end 

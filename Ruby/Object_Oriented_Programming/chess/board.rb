57ed86c8812b0ac7400bc891ebd82e9eb16f9739
Hornet-Developer <hornet.dev.0802@gmail.com>
2022-04-15 22:39:39 -0700

    Add Unicode to board.rb to generate chess piece icons

--- Ruby/Object_Oriented_Programming/chess/board.rb
+++ Ruby/Object_Oriented_Programming/chess/board.rb
@@ -46,7 +46,7 @@ class Board
 
     private 
 
-    attr_accessor :null_piece
+    attr_reader :null_piece
 
     def set_null_pieces
         (2..5).each do |row| 
@@ -54,53 +54,72 @@ class Board
         end 
     end 
 
+    #method translates chess piece unicode into a chess icon 
+    def unicode(code) 
+        code.encode('utf-8')
+    end 
+
     def set_pieces(color)
+        #unicode for chess pieces 
+        white_king = "\u2654"
+        white_queen = "\u2655"
+        white_rook = "\u2656"
+        white_bishop = "\u2657"
+        white_knight = "\u2658"
+        white_pawn = "\u2659"
+        black_king = "\u265A"
+        black_queen = "\u265B"
+        black_rook = "\u265C"
+        black_bishop = "\u265D"
+        black_knight = "\u265E"
+        black_pawn = "\u265F"
+
         #set rooks 
         if color == :white 
-            self[7, 0] = Rook.new(:white, 'R', [7, 0], self)
-            self[7, 7] = Rook.new(:white, 'R', [7, 7], self)
+            self[7, 0] = Rook.new(:white, unicode(white_rook), [7, 0], self)
+            self[7, 7] = Rook.new(:white, unicode(white_rook), [7, 7], self)
         else 
-            self[0, 0] = Rook.new(:black, 'R', [0, 0], self)
-            self[0, 7] = Rook.new(:black, 'R', [0, 7], self)
+            self[0, 0] = Rook.new(:black, unicode(black_rook), [0, 0], self)
+            self[0, 7] = Rook.new(:black, unicode(black_rook), [0, 7], self)
         end 
 
         #set knights 
         if color == :white 
-            self[7, 1] = Knight.new(:white, 'Kn', [7, 1], self)
-            self[7, 6] = Knight.new(:white, 'Kn', [7, 6], self)
+            self[7, 1] = Knight.new(:white, unicode(white_knight), [7, 1], self)
+            self[7, 6] = Knight.new(:white, unicode(white_knight), [7, 6], self)
         else 
-            self[0, 1] = Knight.new(:black, 'Kn', [0, 1], self)
-            self[0, 6] = Knight.new(:black, 'Kn', [0, 6], self)
+            self[0, 1] = Knight.new(:black, unicode(black_knight), [0, 1], self)
+            self[0, 6] = Knight.new(:black, unicode(black_knight), [0, 6], self)
         end 
 
         #set bishops 
         if color == :white 
-            self[7, 2] = Bishop.new(:white, 'B', [7, 2], self)
-            self[7, 5] = Bishop.new(:white, 'B', [7, 5], self)
+            self[7, 2] = Bishop.new(:white, unicode(white_bishop), [7, 2], self)
+            self[7, 5] = Bishop.new(:white, unicode(white_bishop), [7, 5], self)
         else 
-            self[0, 2] = Bishop.new(:black, 'B', [0, 2], self)
-            self[0, 5] = Bishop.new(:black, 'B', [0, 5], self)
+            self[0, 2] = Bishop.new(:black, unicode(black_bishop), [0, 2], self)
+            self[0, 5] = Bishop.new(:black, unicode(black_bishop), [0, 5], self)
         end 
 
         #set queen 
         if color == :white 
-            self[7, 3] = Queen.new(:white, 'Q', [7, 3], self)
+            self[7, 3] = Queen.new(:white, unicode(white_queen), [7, 3], self)
         else 
-            self[0, 3] = Queen.new(:black, 'Q', [0, 3], self)
+            self[0, 3] = Queen.new(:black, unicode(black_queen), [0, 3], self)
         end 
 
         #set king 
         if color == :white 
-            self[7, 4] = King.new(:white, 'K', [7, 4], self)
+            self[7, 4] = King.new(:white, unicode(white_king), [7, 4], self)
         else 
-            self[0, 4] = King.new(:black, 'K', [0, 4], self)
+            self[0, 4] = King.new(:black, unicode(black_king), [0, 4], self)
         end 
 
         #set pawns 
         if color == :white 
-            (0..7).each { |col| self[6, col] = Pawn.new(:white, 'p', [6, col], self) }  
+            (0..7).each { |col| self[6, col] = Pawn.new(:white, unicode(white_pawn), [6, col], self) }  
         else 
-            (0..7).each { |col| self[1, col] = Pawn.new(:black, 'p', [1, col], self) }  
+            (0..7).each { |col| self[1, col] = Pawn.new(:black, unicode(black_pawn), [1, col], self) }  
         end 
     end 
 

3ea4b269294366015f0a5f9d85cdf3bf1031b0ef
Hornet-Developer <hornet.dev.0802@gmail.com>
2022-05-03 20:34:15 -0700

    Complete First TDD Projects

--- Ruby/RSpec_and_TDD/first_tdd_projects/lib/first_tdd_projects.rb
+++ Ruby/RSpec_and_TDD/first_tdd_projects/lib/first_tdd_projects.rb
@@ -7,25 +7,19 @@ end
 
 class Array 
     #method returns the positions in an array that sum to 0. puts positions with smaller indices before larger indices. 
-    def two_sum 
+    def two_sum
         positions_that_sum_to_0 = []
-
-        idx1 = 0 
-        while idx1 < self.length - 1 
-            num1 = self[idx1]
-
-            idx2 = idx1 + 1 
-            while idx2 < self.length 
-                num2 = self[idx2]
-                positions_that_sum_to_0 << [idx1, idx2] if num1 + num2 == 0 
-                idx2 += 1 
-            end 
-
-            idx1 += 1 
+    
+        0.upto(self.length - 2) do |idx1| 
+            (idx1 + 1).upto(self.length - 1) do |idx2| 
+                if self[idx1] + self[idx2] == 0 
+                    positions_that_sum_to_0 << [idx1, idx2]
+                end 
+            end  
         end 
 
-        positions_that_sum_to_0
-    end   
+        positions_that_sum_to_0 
+    end  
 end 
 
 #method returns boolean of whether argument is a 2D array 
@@ -96,4 +90,177 @@ def stock_picker(prices)
     end 
 
     trade_stock(buy_day, sell_day, largest_profit)
+end 
+
+#Towers of Hanoi Game 
+class TowersOfHanoi
+
+    attr_accessor :tower1, :tower2, :tower3, :user_move_from_tower
+
+    def initialize
+        @tower1 = [8, 7, 6, 5, 4, 3, 2, 1]
+        @tower2 = []
+        @tower3 = []
+        @entry_description = nil 
+        @user_move_from_tower = nil 
+        @user_move_to_tower = nil 
+        @disk_to_be_moved = nil 
+        @disk_at_tower_to_be_moved_to = nil 
+        @tower_hash = { 1 => tower1, 2 => tower2, 3 => tower3 }
+        #introduction 
+    end 
+
+    def introduction 
+        puts "Welcome to the Towers of Hanoi!"
+        puts "Read about the game at https://en.wikipedia.org/wiki/Tower_of_Hanoi"
+        puts "You win when you move all the disks to Tower 2 or Tower 3."
+        puts "The disks are numbered by size."
+        puts "A disk can't be placed on a disk that is smaller than it."
+        puts "Good luck!"
+        puts "\n"
+        puts "Press return/enter to begin."
+        gets 
+        play 
+    end 
+
+    def play 
+        until won? 
+            clear 
+            print_towers 
+            user_move
+        end 
+        win
+    end 
+
+    def user_entry
+        valid_entry = false 
+        while !valid_entry 
+            puts "Enter a tower to #{entry_description}. Enter 1, 2, or 3."
+            user_move_from_tower = gets.chomp.to_i 
+            if valid_entry?(user_move_from_tower)
+                valid_entry = true 
+            else 
+                puts "Please enter 1, 2, or 3."
+            end 
+        end 
+        user_move_from_tower
+    end 
+
+    #private 
+
+    attr_reader :disk_to_be_moved, :disk_at_tower_to_be_moved_to, :entry_description, :tower_hash, :user_move_from_tower, :user_move_to_tower
+
+    def tower_empty?(tower)
+        tower_hash[tower].empty?
+    end 
+
+    def disk_at_tower(tower)
+        tower_hash[tower][-1]
+    end 
+
+    def valid_move? 
+        tower_empty?(user_move_to_tower) || disk_to_be_moved <= disk_at_tower_to_be_moved_to
+    end 
+
+    def valid_entry?(tower)
+        [1, 2, 3].include?(tower)
+    end 
+
+    def user_move 
+        user_move_from_tower = get_user_move_from_tower 
+        user_move_to_tower = get_user_move_to_tower 
+        move_disk(user_move_from_tower, user_move_to_tower)
+    end 
+
+    #method moves a disk from towerA to towerB
+    def move_disk(towerA, towerB)
+        unless tower_empty?(towerB)
+            raise "A disk can't be put on a smaller disk." if disk_at_tower(towerA) > disk_at_tower(towerB)
+        end 
+
+        disk = tower_hash[towerA].pop
+        tower_hash[towerB] << disk 
+    end 
+
+    def get_user_move_to_tower
+        @disk_to_be_moved = disk_at_tower(user_move_from_tower)
+        @entry_description = "move disk #{disk_to_be_moved} to"
+
+        valid_move_to_tower = false 
+        while !valid_move_to_tower
+            @user_move_to_tower = user_entry
+            @disk_at_tower_to_be_moved_to = disk_at_tower(user_move_to_tower)
+            if valid_move?
+                valid_move_to_tower = true 
+            else 
+                puts "Disk #{disk_to_be_moved} can't be moved to Tower #{user_move_to_tower}, because disk #{disk_to_be_moved} is larger than disk #{disk_at_tower_to_be_moved_to}." 
+            end 
+        end 
+
+        user_move_to_tower
+    end 
+
+    def get_user_move_from_tower
+        @entry_description = 'move from'
+
+        valid_move_from_tower = false 
+        while !valid_move_from_tower
+            @user_move_from_tower = user_entry
+            if tower_empty?(user_move_from_tower)  
+                puts "Tower #{user_move_from_tower} is empty. Please select a tower that has a disk."
+            else 
+                valid_move_from_tower = true 
+            end 
+        end 
+
+        user_move_from_tower
+    end 
+
+    def win 
+        clear 
+        print_towers
+        puts "Congratulations! You won the Towers of Hanoi!"
+    end 
+
+    def won? 
+        return true if tower2 == [8, 7, 6, 5, 4, 3, 2, 1] || tower3 == [8, 7, 6, 5, 4, 3, 2, 1]
+        false 
+    end 
+
+    def get_longest_tower
+        longest_tower = nil 
+        tower_hash.values.each { |tower| longest_tower = tower if longest_tower == nil || tower.length > longest_tower.length } 
+        longest_tower
+    end 
+
+    def empty_space?(tower, idx)
+        tower[idx] == nil 
+    end 
+
+    def print_towers
+        longest_tower = get_longest_tower
+
+        puts "Towers of Hanoi".rjust(28)
+        idx = longest_tower.length - 1
+        while idx >= 0
+            if empty_space?(tower1, idx) && empty_space?(tower2, idx)
+                puts "#{tower1[idx]}                  #{tower2[idx]}                 #{tower3[idx]}"
+            elsif empty_space?(tower1, idx)
+                puts "#{tower1[idx]}                   #{tower2[idx]}               #{tower3[idx]}"
+            elsif empty_space?(tower1, idx) && empty_space?(tower3, idx)
+                puts "#{tower1[idx]}                   #{tower2[idx]}               #{tower3[idx]}"
+            elsif empty_space?(tower2, idx)
+                puts "#{tower1[idx]}                  #{tower2[idx]}                #{tower3[idx]}"
+            else 
+                puts "#{tower1[idx]}                  #{tower2[idx]}               #{tower3[idx]}"
+            end 
+            idx -= 1 
+        end 
+        puts "Tower 1         Tower 2         Tower 3"
+    end 
+
+    def clear 
+        system("clear")
+    end     
+
 end 
--- Ruby/RSpec_and_TDD/first_tdd_projects/spec/first_tdd_projects_spec.rb
+++ Ruby/RSpec_and_TDD/first_tdd_projects/spec/first_tdd_projects_spec.rb
@@ -2,13 +2,14 @@ require 'rspec'
 require 'first_tdd_projects'
 
 describe '#my_uniq' do 
-    #look for AA solutuon on taking in a parameter...how do they do that?
+    let(:array) { [1, 1, 2, 2, 3, 3] }
+   
     it 'takes in an array' do 
-        expect { my_uniq([1, 2, 3]) }.to_not raise_error
+        expect { my_uniq(array) }.to_not raise_error
     end 
        
     it 'returns array with unique elements in the order they appear in the array' do 
-        expect(my_uniq([1, 1, 2, 2, 3, 3])).to eq([1, 2, 3])
+        expect(my_uniq(array)).to eq([1, 2, 3])
     end 
 end 
 
@@ -19,8 +20,16 @@ describe 'Array' do
         end 
 
         it 'sorts pairs with the smaller index before the bigger index' do 
-            expect([9, 9, -9].two_sum). to eq([[0, 2], [1, 2]])
-            expect([5, -5, -5].two_sum). to eq([[0, 1], [0, 2]])
+            expect([9, 9, -9].two_sum).to eq([[0, 2], [1, 2]])
+            expect([5, -5, -5].two_sum).to eq([[0, 1], [0, 2]])
+        end 
+
+        it 'returns an empty array when there are no pairs of positions that sum to 0' do 
+            expect([0, 5, 9].two_sum).to eq([])
+        end 
+
+        it 'handles an array with two zeros' do 
+            expect([0, 5, 9, 0].two_sum).to eq([[0, 3]])
         end 
     end     
 end 
@@ -57,5 +66,31 @@ describe '#stock_picker' do
     it 'returns an empty array if there are no days where a profit can be made' do 
         expect(stock_picker([15, 14, 12, 5])).to eq([])
         expect(stock_picker([15, 15, 15])).to eq([])
+    end 
+end 
+
+describe '#TowersOfHanoi' do 
+    subject(:game) { TowersOfHanoi.new }
+
+    it 'starts with all disks on rod 1' do 
+        expect(game.tower1).to eq([8, 7, 6, 5, 4, 3, 2, 1])
+    end 
+
+    it 'moves a disk from one rod to another rod' do 
+        game.move_disk(1, 2)
+        expect(game.tower1).to eq([8, 7, 6, 5, 4, 3, 2])
+        expect(game.tower2).to eq([1])
+    end 
+
+    it 'raises an error if a disk is placed on a smaller disk' do 
+        game.move_disk(1, 2)
+        expect{ game.move_disk(1, 2) }.to raise_error("A disk can't be put on a smaller disk.")
+    end 
+
+    it 'returns true when the game is won' do 
+        game.tower1 = []
+        game.tower2 = [8, 7, 6, 5, 4, 3, 2, 1]
+        game.tower3 = []
+        expect(game.won?).to be(true)
     end 
 end 

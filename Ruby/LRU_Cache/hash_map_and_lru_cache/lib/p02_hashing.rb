37f94cd7e0375c43beec369d810459af365632bf
Hornet-Developer <hornet.dev.0802@gmail.com>
2022-06-14 21:31:39 -0700

    p02_hashing.rb complete

--- Ruby/LRU_Cache/hash_map_and_lru_cache/lib/p02_hashing.rb
+++ Ruby/LRU_Cache/hash_map_and_lru_cache/lib/p02_hashing.rb
@@ -4,18 +4,24 @@ end
 
 class Array
   def hash
+    integer_from_array = self.join.to_i
+    Integer(integer_from_array).hash 
   end
 end
 
 class String
   def hash
+    integer_string = ''
+    ltr_and_num_arr = ('a'..'z').to_a + ('0'..'9').to_a
+    self.each_char { |char| integer_string << ltr_and_num_arr.index(char).to_s }
+    Integer(integer_string.to_i).hash 
   end
 end
 
 class Hash
-  # This returns 0 because rspec will break if it returns nil
-  # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    string_from_hash = self.to_a.sort.join
+    String(string_from_hash).hash 
   end
 end
+

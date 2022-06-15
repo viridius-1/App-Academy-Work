dcb8425d86563c269112ae5fd8a40ecab8a3c346
Hornet-Developer <hornet.dev.0802@gmail.com>
2022-06-15 15:31:17 -0700

    p03_hash_set.rb complete

--- Ruby/LRU_Cache/hash_map_and_lru_cache/lib/p03_hash_set.rb
+++ Ruby/LRU_Cache/hash_map_and_lru_cache/lib/p03_hash_set.rb
@@ -1,5 +1,6 @@
 class HashSet
-  attr_reader :count
+
+  attr_reader :count, :store 
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
@@ -7,24 +8,52 @@ class HashSet
   end
 
   def insert(key)
+    resize! if resize_needed?
+    bucket = get_bucket(key)
+    unless self[bucket].include?(key)
+      self[bucket] << key  
+      @count += 1 
+    end 
   end
 
   def include?(key)
+    bucket = get_bucket(key)
+    self[bucket].include?(key) 
   end
 
   def remove(key)
+    bucket = get_bucket(key)
+    @count -= 1 if store[bucket].delete(key)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    store[num] 
   end
 
   def num_buckets
     @store.length
   end
 
+  def get_bucket(key)
+    key.hash % num_buckets
+  end 
+
+  def resize_needed? 
+    count != 0 && count % 8 == 0 
+  end 
+
   def resize!
+    store_copy = store.dup 
+    @store = Array.new(store.length * 2) { Array.new }
+
+    store_copy.each do |old_bucket| 
+      old_bucket.each do |key| 
+        bucket = get_bucket(key)
+        self[bucket] << key  
+      end 
+    end 
   end
+
 end

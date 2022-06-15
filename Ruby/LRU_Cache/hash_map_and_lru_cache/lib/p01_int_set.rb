08c22fa9c637265d2922290bc1f91c1e470050c8
Hornet-Developer <hornet.dev.0802@gmail.com>
2022-06-14 18:58:15 -0700

    ResizingIntSet class complete

--- Ruby/LRU_Cache/hash_map_and_lru_cache/lib/p01_int_set.rb
+++ Ruby/LRU_Cache/hash_map_and_lru_cache/lib/p01_int_set.rb
@@ -60,7 +60,7 @@ class IntSet
 end
 
 class ResizingIntSet
-  attr_reader :count
+  attr_reader :count, :store 
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
@@ -68,24 +68,51 @@ class ResizingIntSet
   end
 
   def insert(num)
+    resize! if resize_needed? 
+    bucket = get_bucket(num)
+    unless self[bucket].include?(num)
+      self[bucket] << num 
+      @count += 1 
+    end 
   end
 
   def remove(num)
+    bucket = get_bucket(num)
+    @count -= 1 if store[bucket].delete(num)
   end
 
   def include?(num)
+    bucket = get_bucket(num)
+    self[bucket].include?(num) 
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    store[num]
   end
 
   def num_buckets
     @store.length
   end
 
+  def get_bucket(num)
+    num % num_buckets
+  end 
+
+  def resize_needed?
+    count != 0 && count % 20 == 0 
+  end 
+
   def resize!
+    store_copy = store.dup 
+    @store = Array.new(store.length * 2) { Array.new }
+
+    store_copy.each do |old_bucket| 
+      old_bucket.each do |el| 
+        bucket = get_bucket(el)
+        self[bucket] << el 
+      end 
+    end 
   end
 end

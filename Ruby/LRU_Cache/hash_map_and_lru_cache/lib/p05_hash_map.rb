c3200b73abb04033359dbe1279915e36db66bebd
Hornet-Developer <hornet.dev.0802@gmail.com>
2022-06-16 11:41:33 -0700

    p05_hash_map.rb complete

--- Ruby/LRU_Cache/hash_map_and_lru_cache/lib/p05_hash_map.rb
+++ Ruby/LRU_Cache/hash_map_and_lru_cache/lib/p05_hash_map.rb
@@ -1,7 +1,10 @@
 require_relative 'p04_linked_list'
 
 class HashMap
-  attr_accessor :count
+  
+  include Enumerable
+
+  attr_accessor :count, :store 
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { LinkedList.new }
@@ -9,27 +12,47 @@ class HashMap
   end
 
   def include?(key)
+    bucket, linked_list = get_bucket_and_linked_list(key)
+    return true if linked_list.find(key)
+    false 
   end
 
   def set(key, val)
+    resize! if resize_needed?
+    bucket, linked_list = get_bucket_and_linked_list(key)
+
+    if include?(key) 
+      node = linked_list.find(key)
+      node.val = val 
+    else 
+      linked_list.append(key, val)
+      @count += 1 
+    end 
   end
 
   def get(key)
+    bucket, linked_list = get_bucket_and_linked_list(key)
+    linked_list.get(key)
   end
 
   def delete(key)
+    bucket, linked_list = get_bucket_and_linked_list(key)
+    linked_list.remove(key)
+    @count -= 1 
   end
 
-  def each
+  def each(&prc)
+    store.each do |linked_list| 
+      linked_list.each { |node| prc.call(node.key, node.val) } 
+    end 
   end
 
-  # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -40,10 +63,28 @@ class HashMap
     @store.length
   end
 
+  def resize_needed? 
+    count != 0 && count % 8 == 0 
+  end 
+
   def resize!
+    store_copy = store.dup 
+    @store = Array.new(store.length * 2) { LinkedList.new }
+    @count = 0 
+
+    store_copy.each do |old_linked_list| 
+      old_linked_list.each { |node| set(node.key, node.val) } 
+    end 
   end
 
   def bucket(key)
-    # optional but useful; return the bucket corresponding to `key`
+    key.hash % num_buckets
   end
+
+  def get_bucket_and_linked_list(key)
+    bucket = bucket(key)
+    linked_list = store[bucket]
+    [bucket, linked_list]
+  end 
+
 end

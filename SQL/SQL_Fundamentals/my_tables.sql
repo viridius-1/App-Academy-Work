89b088b2cba7a0d593e06ba2a3d13acd702f16f0
Hornet-Developer <hornet.dev.0802@gmail.com>
2022-06-21 22:31:36 -0700

    my_tables.sql complete

---
+++ SQL/SQL_Fundamentals/my_tables.sql
@@ -0,0 +1,53 @@
+/* Drop Tables */
+DROP TABLE IF EXISTS animals; 
+DROP TABLE IF EXISTS countries; 
+
+
+/* Create Tables */ 
+/* Countries Table */ 
+CREATE TABLE countries(
+     id SERIAL PRIMARY KEY, 
+     name VARCHAR(255) NOT NULL, 
+     population INTEGER NOT NULL
+);
+
+/* Animals Table */ 
+CREATE TABLE animals(
+     id SERIAL PRIMARY KEY, 
+     name VARCHAR(255) NOT NULL, 
+     country_id INTEGER NOT NULL,
+
+     FOREIGN KEY (country_id) REFERENCES countries(id)
+);
+
+
+/* Insert Data */ 
+/* Insert Country Data */
+INSERT INTO
+     countries 
+     (name, population)
+VALUES 
+     ('United States', '327000000')
+; 
+
+/* Insert Animals Data */
+INSERT INTO 
+     animals 
+     (name, country_id)
+VALUES 
+     ('Racoon',
+     (SELECT id 
+      FROM countries 
+      WHERE name = 'United States')), 
+     ('Black Bear',
+     (SELECT id 
+      FROM countries 
+      WHERE name = 'United States')), 
+     ('Bald Eagle',
+     (SELECT id 
+      FROM countries 
+      WHERE name = 'United States'))
+; 
+
+
+

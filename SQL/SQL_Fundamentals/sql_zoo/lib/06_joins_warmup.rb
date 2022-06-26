60d857255cf599e28c1ed288be77ddc10db7378e
Hornet-Developer <hornet.dev.0802@gmail.com>
2022-06-26 15:35:52 -0700

    06_joins_warmup.rb complete

--- SQL/SQL_Fundamentals/sql_zoo/lib/06_joins_warmup.rb
+++ SQL/SQL_Fundamentals/sql_zoo/lib/06_joins_warmup.rb
@@ -48,6 +48,12 @@ end
 def year_of_kane
   # Give year of 'Citizen Kane'.
   execute(<<-SQL)
+    SELECT 
+      yr
+    FROM 
+      movies
+    WHERE 
+      title = 'Citizen Kane'
   SQL
 end
 
@@ -56,24 +62,50 @@ def trek_films
   # these movies include the words Star Trek in the title). Order results by
   # year.
   execute(<<-SQL)
+    SELECT 
+      id, title, yr
+    FROM
+      movies  
+    WHERE 
+      title LIKE '%Star Trek%'
+    ORDER BY 
+      yr
   SQL
 end
 
 def films_by_id
   # What are the titles of the films with id 1119, 1595, 1768?
   execute(<<-SQL)
+    SELECT 
+      title 
+    FROM 
+      movies 
+    WHERE 
+      id IN (1119, 1595, 1768)
   SQL
 end
 
 def glenn_close_id
   # What id number does the actress 'Glenn Close' have?
   execute(<<-SQL)
+    SELECT 
+      id
+    FROM 
+      actors  
+    WHERE 
+      name = 'Glenn Close'
   SQL
 end
 
 def casablanca_id
   # What is the id of the film 'Casablanca'?
   execute(<<-SQL)
+    SELECT 
+      id
+    FROM 
+      movies 
+    WHERE 
+      title = 'Casablanca'
   SQL
 end
 
@@ -81,17 +113,75 @@ def casablanca_cast
   # Obtain the cast list for 'Casablanca'. Use the id value that you obtained
   # in the previous question directly in your query (for example, id = 1).
   execute(<<-SQL)
+    SELECT 
+      name
+    FROM 
+      castings
+    JOIN
+      actors ON castings.actor_id = actors.id 
+    WHERE 
+      movie_id = 27 
   SQL
 end
 
 def alien_cast
   # Obtain the cast list for the film 'Alien'
   execute(<<-SQL)
+    SELECT
+      name 
+    FROM 
+      castings 
+    JOIN 
+      actors ON castings.actor_id = actors.id 
+    WHERE 
+      movie_id = (
+        SELECT
+          id
+        FROM
+          movies 
+        WHERE
+          title = 'Alien' 
+      )
   SQL
 end
 
 
 #FILMS FROM SIXTY TWO 
-puts "FILMS FROM SIXTY TWO "
+puts "FILMS FROM SIXTY TWO"
 p films_from_sixty_two
+puts "-------------------------------------"
+
+#YEAR OF KANE
+puts "YEAR OF KANE"
+p year_of_kane
+puts "-------------------------------------"
+
+#TREK FILMS
+puts "TREK FILMS"
+p trek_films
+puts "-------------------------------------"
+
+#FILMS BY ID 
+puts "FILMS BY ID"
+p trek_films
+puts "-------------------------------------"
+
+#GLENN CLOSE ID 
+puts "GLENN CLOSE ID"
+p glenn_close_id
+puts "-------------------------------------"
+
+#CASABLANCA ID 
+puts "CASABLANCA ID"
+p casablanca_id
+puts "-------------------------------------"
+
+#CASABLANCA CAST 
+puts "CASABLANCA CAST"
+p casablanca_cast
+puts "-------------------------------------"
+
+#ALIEN CAST 
+puts "ALIEN CAST"
+p alien_cast
 puts "-------------------------------------"

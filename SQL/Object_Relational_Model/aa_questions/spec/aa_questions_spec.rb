3ff6c9b71d0c709f08b467bea6cd48d8d610f639
Hornet-Developer <hornet.dev.0802@gmail.com>
2022-08-11 13:49:16 -0700

    Initial commit for specs

---
+++ SQL/Object_Relational_Model/aa_questions/spec/aa_questions_spec.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+require 'active_support/inflector'
+require 'aa_questions'
+
+
+describe User do 
+
+    describe "find_by_name" do 
+        it "finds a user by first and last name" do 
+            user = User.find_by_name("Jake", "Shapiro")
+            expect(user.fname).to eq("Jake")
+            expect(user.lname).to eq("Shapiro")
+        end 
+    end 
+    
+end 

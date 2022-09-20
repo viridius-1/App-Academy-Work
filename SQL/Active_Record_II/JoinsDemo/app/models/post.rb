16f78f7624618378fd410a44e6d2f0ffa7a339f7
Hornet-Developer <hornet.dev.0802@gmail.com>
2022-09-19 20:23:53 -0400

    Add ::by_popularity method to post.rb

--- SQL/Active_Record_II/JoinsDemo/app/models/post.rb
+++ SQL/Active_Record_II/JoinsDemo/app/models/post.rb
@@ -1,7 +1,14 @@
 class Post < ApplicationRecord
-
   validates :title, :body, :author_id, :presence => true
 
   belongs_to :author, :class_name => "User"
   has_many :comments, :foreign_key => :post_id
+
+  def self.by_popularity 
+      self
+        .select('posts.*, COUNT(*) AS comment_count')
+        .joins(:comments)
+        .group('posts.id')
+        .order('comment_count DESC')
+  end 
 end

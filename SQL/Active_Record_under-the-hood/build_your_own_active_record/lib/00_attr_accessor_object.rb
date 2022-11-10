53bd0838bfbb20a3cc0d4626c0385911509e6140
Hornet-Developer <hornet.dev.0802@gmail.com>
2022-11-09 21:19:48 -0800

    Phase 0 complete

---
+++ SQL/Active_Record_under-the-hood/build_your_own_active_record/lib/00_attr_accessor_object.rb
@@ -0,0 +1,14 @@
+class AttrAccessorObject
+
+  def self.my_attr_accessor(*names)
+    names.each do |name| 
+      define_method("#{name}") { self.instance_variable_get("@#{name}") }
+      define_method("#{name}=") { |value| self.instance_variable_set("@#{name}", value) }
+    end 
+  end 
+
+end
+
+
+
+ 

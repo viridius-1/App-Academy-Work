f24bd747fabfffb76aeac907de30f223be0d2d40
Hornet-Developer <hornet.dev.0802@gmail.com>
2022-11-10 08:59:43 -0800

    Phase 1a complete

--- SQL/Active_Record_under-the-hood/build_your_own_active_record/lib/00_attr_accessor_object.rb
+++ SQL/Active_Record_under-the-hood/build_your_own_active_record/lib/00_attr_accessor_object.rb
@@ -2,13 +2,9 @@ class AttrAccessorObject
 
   def self.my_attr_accessor(*names)
     names.each do |name| 
-      define_method("#{name}") { self.instance_variable_get("@#{name}") }
-      define_method("#{name}=") { |value| self.instance_variable_set("@#{name}", value) }
+      define_method("#{name}") { instance_variable_get("@#{name}") }
+      define_method("#{name}=") { |value| instance_variable_set("@#{name}", value) }
     end 
   end 
 
 end
-
-
-
- 
--- SQL/Active_Record_under-the-hood/build_your_own_active_record/lib/01_sql_object.rb
+++ SQL/Active_Record_under-the-hood/build_your_own_active_record/lib/01_sql_object.rb
@@ -12,11 +12,24 @@ class SQLObject
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
+
+    # sets the table 
+    #Since our SQLObject class is itself an object (an instance of Class, 
+      #we can store that as a instance variable on the class #everythingisanobject.
   end
 
   def self.table_name
-    # ...
+    if @table_name.nil?  
+      self.table_name = self.to_s.tableize
+    else 
+      @table_name
+    end    
+    # gets the name of the table for the class
+    #It would also be nice if, in the absence of an explicitly set table name, 
+      #we would have ::table_name by default convert the class name to snake_case and pluralize:
+      #ActiveSupport (part of Rails) has an inflector library that adds methods to String to help you do this.
+      # In particular, look at the String#tableize method. You can require the inflector with require 'active_support/inflector'
   end
 
   def self.all

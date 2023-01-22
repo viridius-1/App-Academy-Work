a83f07da5ca5a6a569ba393f83a53f5208e08107
Hornet-Developer <hornet.dev.0802@gmail.com>
2023-01-21 18:29:08 -0800

    Updated seeds file

--- Rails/test_project/db/seeds.rb
+++ Rails/test_project/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Superhero.destroy_all
+Superhero.create(name: "Superman", secret_identity: "Clark Kent", power: 200)
+Superhero.create(name: "Powdered toast man", secret_identity: "Tom Toast", power: 160)
+Superhero.create(name: "Spiderman", secret_identity: "Peter Parker", power: 78)

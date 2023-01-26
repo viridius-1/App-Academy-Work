3e149e252e84bec1799925b48229fdf3962a779d
Hornet-Developer <hornet.dev.0802@gmail.com>
2023-01-25 23:58:41 -0800

    Updated project_euler.rb

--- Ruby/Project_Euler/project_euler.rb
+++ Ruby/Project_Euler/project_euler.rb
@@ -1,4 +1,4 @@
-#Problem 1 - method returns the sum of multiples of 3 or 5 below n.
+#Problem 1 - method returns the sum of multiples of 3 or 5 below a number, n.
 def mults_sum(n)
     sum = 0 
     (2...n).each { |i| sum += i if i % 3 == 0 || i % 5 == 0 } 

389a3aceedb6afaf4563d56cef17c24fae8abfbe
Hornet-Developer <hornet.dev.0802@gmail.com>
2023-01-29 00:24:15 -0800

    Updated superheroes_controller.rb

--- Rails/test_project/app/controllers/superheroes_controller.rb
+++ Rails/test_project/app/controllers/superheroes_controller.rb
@@ -9,10 +9,20 @@ class SuperheroesController < ApplicationController
         render json: superhero
     end 
 
+    def create 
+        superhero = Superhero.new(superhero_params)
+
+        if superhero.save 
+            render json: superhero
+        else 
+            render json: superhero.errors.full_messages, status: :unprocessable_entity 
+        end 
+    end 
+
     def update 
         superhero = Superhero.find_by(id: params[:id])
 
-        if superhero.update(superhero_params)
+        if superhero.update(params[:superhero])
             render json: superhero 
         else  
             render json: superhero.errors.full_messages, status: :unprocessable_entity 

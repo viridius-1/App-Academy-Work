a714e0b80195ebec6951bb18ad55d605b69ae534
Hornet-Developer <hornet.dev.0802@gmail.com>
2023-02-03 20:52:42 -0800

    Updated users_controller.rb

--- Rails/Routes_and_Controllers/first_routes_and_controllers/app/controllers/users_controller.rb
+++ Rails/Routes_and_Controllers/first_routes_and_controllers/app/controllers/users_controller.rb
@@ -1,7 +1,38 @@
 class UsersController < ApplicationController 
 
     def index
-        render plain: "I'm in the index action!"
+        render json: User.all 
+    end 
+
+    def create
+        user = User.new(user_params) 
+        render json: user if user.save! 
+    end 
+
+    def show 
+        user = User.find(params[:id])
+        render json: user if user 
+    end 
+
+    def update 
+        user = User.find(params[:id])
+        render json: user if user.update(user_params)
+    end 
+
+    def destroy 
+        user = User.find(params[:id])
+        
+        if user.destroy 
+            render json: user
+        else 
+            puts "Can't delete this user!"
+        end 
+    end 
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:name, :email)
     end 
 
 end 

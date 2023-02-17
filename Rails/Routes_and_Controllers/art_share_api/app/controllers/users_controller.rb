1b09e4980f034d7a876ee848ae988b8c6ab0c0e7
Hornet-Developer <hornet.dev.0802@gmail.com>
2023-02-16 20:17:45 -0800

    Updated users_controller#index to add search by name functionality

--- Rails/Routes_and_Controllers/art_share_api/app/controllers/users_controller.rb
+++ Rails/Routes_and_Controllers/art_share_api/app/controllers/users_controller.rb
@@ -2,7 +2,19 @@ class UsersController < ApplicationController
     verify_authenticity_token
 
     def index
-        render json: User.all 
+        if params[:username]
+            render json: User.where(username: params[:username])
+        elsif query_in_request_params? 
+            name = get_name_from_query
+            user = User.select(:id, :username, :created_at, :updated_at).where(username: name)
+            if user != []
+                render json: user 
+            else   
+                render plain: "User not in database."
+            end 
+        else 
+            render json: User.all
+        end 
     end 
 
     def create
@@ -41,4 +53,12 @@ class UsersController < ApplicationController
         params.require(:user).permit(:username)
     end 
 
+    def query_in_request_params?
+        params.each { |key, value| return true if !["controller", "action", "username"].include?(key) } 
+        false 
+    end 
+
+    def get_name_from_query
+        params.each { |key, value| return key if key != "username" } 
+    end 
 end 
